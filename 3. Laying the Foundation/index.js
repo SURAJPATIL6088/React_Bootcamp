5c157a3b06d6dbdef6a68948094c6551a5dbc9c9
SURAJ BALARAM PATIL <78692972+SURAJPATIL6088@users.noreply.github.com>
2023-06-07 18:01:54 +0530

    Update index.js

--- 3. Laying the Foundation/index.js
+++ 3. Laying the Foundation/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+/*
 const createElement = React.createElement;
-
 const chapterHeading = createElement
 (
     "h1",
@@ -39,7 +39,40 @@ const divContainer = createElement
     },
     [chapterHeading, heading1, heading2],
 );
+*/
+
+// using functional component
+// normal js variable
+const chapterHeading = (
+  <h1 id="title" key="chapterheading">
+    Welcome, to the Chapter 3 of the Series
+  </h1>
+);
+
+const heading1 = (
+  <h2 id="title" key="heading1">
+    Heading 1 in react using Functional Component
+  </h2>
+);
+
+const heading2 = (
+  <h2 id="title" key="heading2">
+    Heading 2 in react using Functional Component
+  </h2>
+);
+
+// functional component
+const DivContainer = () => {
+  return (
+    <div>
+      {chapterHeading}
+      {heading1}
+      {heading2}
+      <h2>Namaste React </h2>
+      <h2>Hello React </h2>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("container"));
-
 root.render(divContainer);
