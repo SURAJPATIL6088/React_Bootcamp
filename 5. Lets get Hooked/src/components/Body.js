c169506874ffece6ad9819addff395ac1a2addcf
SURAJ BALARAM PATIL <78692972+SURAJPATIL6088@users.noreply.github.com>
2023-06-14 16:45:37 +0530

    Update Body.js
    Added search functionality

--- 5. Lets get Hooked/src/components/Body.js
+++ 5. Lets get Hooked/src/components/Body.js
@@ -1,14 +1,56 @@
-import {Restrauntlist} from "./constants";
+import { Restrauntlist } from "./constants";
 import RestrauntCard from "./RestrauntCard";
+import { useState } from "react";
+
+function filterData(SearchInput, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant.data.name.includes(SearchInput)
+  );
+
+  return filterData;
+}
 
 const BodyComponent = () => {
-    return (
+  // create the hooks for storing the local variable
+
+  // const[variable name, function to update the variable]
+  const [restaurants, setRestaurants] = useState(Restrauntlist);
+  const [SearchInput, setSearchInput] = useState();
+
+  return (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={SearchInput}
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+          }}
+        />
+        <button
+          className="search_btn"
+          onClick={() => {
+            // need to filter the data
+            const data = filterData(SearchInput, restaurants);
+
+            // update the state - restaurants
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
       <div className="RestrauntCard-list">
-        {Restrauntlist.map((restaurant) => {
-          return <RestrauntCard {...restaurant.data} key = {...restaurant.data.id} />
+        {restaurants.map((restaurant) => {
+          return (
+            <RestrauntCard {...restaurant.data} key={restaurant.data.id} />
+          );
         })}
       </div>
-    );
-  };
+    </>
+  );
+};
 
 export default BodyComponent;
