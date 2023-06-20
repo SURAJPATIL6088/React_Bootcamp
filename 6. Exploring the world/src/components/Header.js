42a2e6a184cd281d28b67ce7111e6e8ecbca8753
SURAJ BALARAM PATIL <78692972+SURAJPATIL6088@users.noreply.github.com>
2023-06-20 18:11:06 +0530

    Update Header.js

--- 6. Exploring the world/src/components/Header.js
+++ 6. Exploring the world/src/components/Header.js
@@ -1,28 +1,42 @@
+import { useState } from "react";
+
+// const loggedInUser = () => {
+//   // API call to check authentication
+//   return true;
+// };
+
 export const Title = () => (
-    <a href="/">
-      <img
-        className="logo"
-        alt="food_villa_logo"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpbS295BL3xdJwXWwh7cTytVNtCwa-0r5TI9UviYdDmsnDk15ovoj2HcZlusgDiYkuo4o&usqp=CAU"
-      />
-    </a>
-  );
-  
+  <a href="/">
+    <img
+      className="logo"
+      alt="food_villa_logo"
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpbS295BL3xdJwXWwh7cTytVNtCwa-0r5TI9UviYdDmsnDk15ovoj2HcZlusgDiYkuo4o&usqp=CAU"
+    />
+  </a>
+);
+
 const HeaderComponent = () => {
-    return (
-      <div className="header">
-        <Title />
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Cart</li>
-          </ul>
-        </div>
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
       </div>
-    );
-  };
+      {isLoggedIn ? (
+        <button onClick={() => setIsLoggedIn(false)}>Logout</button>
+      ) : (
+        <button onClick={() => setIsLoggedIn(true)}>Login</button>
+      )}
+    </div>
+  );
+};
 
 // export the header
 export default HeaderComponent;
