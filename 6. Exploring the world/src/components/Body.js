ea5fe277c21bd788e13257f8c625f251dba69e6d
SURAJ BALARAM PATIL <78692972+SURAJPATIL6088@users.noreply.github.com>
2023-06-20 18:09:45 +0530

    Update Body.js

--- 6. Exploring the world/src/components/Body.js
+++ 6. Exploring the world/src/components/Body.js
@@ -1,10 +1,12 @@
 import { Restrauntlist } from "./constants";
 import RestrauntCard from "./RestrauntCard";
 import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+import { swiggy_api_URL } from "./constants";
 
 function filterData(SearchInput, restaurants) {
   const filterData = restaurants.filter((restaurant) =>
-    restaurant.data.name.includes(SearchInput)
+    restaurant?.data?.name?.toLowerCase()?.includes(SearchInput.toLowerCase())
   );
 
   return filterData;
@@ -14,23 +16,34 @@ const BodyComponent = () => {
   // create the hooks for storing the local variable
 
   // const[variable name, function to update the variable]
-  const [restaurants, setRestaurants] = useState(Restrauntlist);
   const [SearchInput, setSearchInput] = useState();
+  const [allRestaurants, setAllRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  
 
-  useEffect(() =>{
+  useEffect(() => {
     // API call
-    getRestaurants();  
-  }, [])
+    getRestaurants();
+  }, []);
 
   // to get fetch data from the API
-  async function getRestaurants(){
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&page_type=DESKTOP_WEB_LISTING");
+  async function getRestaurants() {
+    const data = await fetch(swiggy_api_URL);
     const json = await data.json();
     console.log(json);
 
     // set in the setRestaurant Function
-    setRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
   }
+
+  // Early return 
+  if(!allRestaurants) return null;
+
+  // if(filteredRestaurants?.length === 0)
+  // {
+  //   return <h1>No match found !!</h1>
+  // }                                                
 
   return (
     <>
@@ -48,22 +61,28 @@ const BodyComponent = () => {
           className="search-btn"
           onClick={() => {
             // need to filter the data
-            const data = filterData(SearchInput, restaurants);
+            const data = filterData(SearchInput, allRestaurants);
 
             // update the state - restaurants
-            setRestaurants(data);
+            setFilteredRestaurants(data);
           }}
         >
           Search
         </button>
       </div>
+      {allRestaurants?.length === 0 ? (
+        <Shimmer/>
+      ) : (
       <div className="RestrauntCard-list">
-        {restaurants.map((restaurant) => {
+        {/* have to write the logic if no restaurants are found */}
+
+        {filteredRestaurants.map((restaurant) => {
           return (
             <RestrauntCard {...restaurant.data} key={restaurant.data.id} />
           );
         })}
       </div>
+      )}
     </>
   );
 };
