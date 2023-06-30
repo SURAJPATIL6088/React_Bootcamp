cc02e87b4cf3215939abd0daf0842efc09734f64
SURAJ BALARAM PATIL <78692972+SURAJPATIL6088@users.noreply.github.com>
2023-06-30 17:38:16 +0530

    Delete 10. Jo dikhta hai vo bikta hai directory

--- 10. Jo dikhta hai vo bikta hai/src/utils/helper.js
+++
@@ -1,9 +0,0 @@
-function filterData(SearchInput, restaurants) {
-  const filterData = restaurants.filter((restaurant) =>
-    restaurant?.data?.name?.toLowerCase()?.includes(SearchInput.toLowerCase())
-  );
-
-  return filterData;
-}
-
-export default filterData;
