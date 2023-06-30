b18303ae22ec2d18b1d14127cefba110e69ee8cb
SURAJ BALARAM PATIL <78692972+SURAJPATIL6088@users.noreply.github.com>
2023-06-30 16:27:38 +0530

    10. Jo dikhta hai vo bikta hai

---
+++ 10. Jo dikhta hai vo bikta hai/src/components/Instamart.js
@@ -0,0 +1,10 @@
+const Instamart = () =>{
+    return (
+        <>
+            <h2>Instamart</h2>
+            <h3>it has the features of instant delivery</h3>
+        </>
+    )
+}
+
+export default Instamart;
